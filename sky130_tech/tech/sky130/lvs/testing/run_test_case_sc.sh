aef21a379668606ea3b8043ab05baef1e2669eff
mohanad0mohamed <mohanad_mohamed@mabrains.com>
2022-09-29 18:19:14 +0200

    Adding all other SC files and modifiying regression for them, Adding GitHub Actions for all

--- sky130_tech/tech/sky130/lvs/testing/run_test_case_sc.sh
+++ sky130_tech/tech/sky130/lvs/testing/run_test_case_sc.sh
@@ -5,19 +5,20 @@ export PDK_ROOT=`readlink -f $2`
 export PDK=$3
 export TESTCASES_DIR=`readlink -f $4`
 export RUN_FOLDER=`readlink -f $5`
+export LIB=$6
 
 echo "## Running test case for $CASE_NAME"
 echo "PDK_ROOT = $PDK_ROOT and PDK = $PDK"
 
-## hd cells
-hd_gds=$TESTCASES_DIR/hd/${CASE_NAME}.gds
-hd_cdl=$TESTCASES_DIR/hd/${CASE_NAME}.cdl
+## Lib cells
+gds_file=${TESTCASES_DIR}/${LIB}/${CASE_NAME}.gds
+cdl_file=${TESTCASES_DIR}/${LIB}/${CASE_NAME}.cdl
 
-if [ -f $hd_gds ]
+if [ -f $gds_file ]
 then
-    if [ -f $hd_cdl ]
+    if [ -f $cdl_file ]
     then
-        python3 $PDK_ROOT/$PDK/run_lvs.py --design=$hd_gds --net=$hd_cdl --output_netlist=$RUN_FOLDER/${CASE_NAME}_pass_ext.cir --report=$RUN_FOLDER/${CASE_NAME}_pass > $RUN_FOLDER/${CASE_NAME}_pass_lvs.log 2>&1
+        python3 $PDK_ROOT/$PDK/run_lvs.py --design=$gds_file --net=$cdl_file --output_netlist=$RUN_FOLDER/${CASE_NAME}_pass_ext.cir --report=$RUN_FOLDER/${CASE_NAME}_pass > $RUN_FOLDER/${CASE_NAME}_pass_lvs.log 2>&1
         return_code=$?
         if [ "$return_code" != "0" ]
         then
@@ -28,10 +29,10 @@ then
             echo "## Pass test case $CASE_NAME passed successfully."
         fi
     else
-        echo "## Can't find pass CDL for case: $hd_cdl"
+        echo "## Can't find pass CDL for case: $cdl_file"
         exit 1
     fi
 else
-    echo "## Can't find pass GDS for case: $hd_gds"
+    echo "## Can't find pass GDS for case: $gds_file"
     exit 1
 fi
